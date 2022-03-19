e4d84e188452f229ce2888f80cf5164699583310
peihsuan <lalalahsuan@gmail.com>
2022-03-20 03:43:15 +0800

    note

--- 0318/app0318.js
+++ 0318/app0318.js
@@ -4,19 +4,12 @@
 // console.log(h);
 // box.appendChild(h) //在畫面上把hello加在box後面
 
-
-
-
 // const btn = document.querySelector("#btn")
 // btn.addEventListener("click",() =>{
 //     const lastOne = document.querySelector("#list :last-child")
 //     lastOne.remove()
 //     // console.log(lastOne); //<li>E</li>
 // })
-
-
-
-
 
 // const btn = document.querySelector("#btn")
 // btn.addEventListener("click",() =>{
@@ -25,7 +18,6 @@
 //     lastOne.remove()
 //     console.log(lastOne);
 // })
-
 
 // const btn = document.querySelector("#btn")
 // btn.addEventListener("click",() =>{
@@ -36,57 +28,60 @@
 //     console.log(lastOne);
 // })
 
-
-
-
-
 // const btn = document.querySelector("#btn")
 // btn.addEventListener("click",() =>{
 //     const lastOne = document.querySelector("#list :last-child")
-    
+
 //     console.log(lastOne.parentElement); //找上一層的東西
 //     console.log(lastOne.parentNode);
 // })
 // //E也是一種N 繼承node的功能 E < N (符號<是繼承)
 // //Element大多處理看的到的東西
 
-
 // const btn = document.querySelector("#btn")
 // btn.addEventListener("click",() =>{
 //     const pa = document.querySelector("ul")
 //     console.log(pa.children); //只抓看的到的 印出HTMLCollection(5) [li, li, li, li, li] 不能用forEach
 //     console.log(pa.childNodes); //把看不到的都抓(連同換行) 印出NodeList(11)[text, li, text, li, text, li, text, li, text, li, text]
-    
+
 // })
 
-// const btn = document.querySelector("#btn")
-// btn.addEventListener("click",()=>{
-//     const pa = document.querySelector("ul")
-//     console.log(pa.children); //印出父層ul下的所有子層[]
-//     // for(let i = 0; i < pa.children.length; i++)
-//     // console.log(pa.children[i].textContent);//加.textContent可以只印出值
-//     let papaList = [...pa.children]
-//     papaList.forEach(function(i){
-//         console.log(i.textContent); //印出<li>裡面的值A
-//         console.log(i);//<li>A</li>
-//     })
-// })
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", () => {
+//   const pa = document.querySelector("ul");
+//   console.log(pa.children); //印出父層ul下的所有子層[]
+//   // for(let i = 0; i < pa.children.length; i++)
+//   // console.log(pa.children[i].textContent);//加.textContent可以只印出值
+//   let papaList = [...pa.children];
+//   papaList.forEach(function (i) {
+//     console.log(i.textContent); //印出<li>裡面的值A
+//     console.log(i); //<li>A</li>
+//   });
+// });
 
 // //HTMLCollection沒有forEach功能 NodeList才有
 // //挑戰 把看不到的註解都在HTML呈現 按鈕按下去顯示所有的註解
 
-btn = document.querySelector("#btn")
-btn.addEventListener("click",()=>{
-    const pa = document.querySelector("ul")
-    console.log(pa.children); //印出父層ul下的所有子層[]
-    for(let i = 0; i < pa.children.length; i++)
-    console.log(pa.children[i]);
-    const paList = document.createElement("p")
-    paList.textContent = pa.children
-    pa.appendChild(paList)
-})
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", () => {
+//   const pa = document.querySelector("ul");
+//   const list = document.createElement("li");
+//   //   list.textContent = "hello";
+//   //   pa.appendChild(list);
+//   const paList = document.createElement("li");
+//   paList.textContent = "";
+//   console.log(pa.children); //印出父層ul下的所有子層[]
+//   for (let i = 0; i < pa.children.length; i++) {
+//     console.log(pa.children[i].textContent);
+//     paList.textContent = pa.children[i];
+//     // console.log(paList);
+//     // pa.appendChild(paList);
+//   }
+//   //   console.log(paList);
 
-
+//   //   console.log(paList);
+//   pa.appendChild(paList);
+// });
 
 // const lastOne = document.querySelector("ul :last-child")
 
@@ -94,8 +89,6 @@ btn.addEventListener("click",()=>{
 // console.log(lastOne.previousSibling); //取上一個兄弟姊妹 是空格text
 // console.log(lastOne.nextElementSibling); //取下一個兄弟null
 // console.log(lastOne.nextSibling); //下一個兄弟姊妹 空格
-
-
 
 // const list = document.querySelector("#list")
 // const newList = document.createElement("li")
@@ -110,4 +103,3 @@ btn.addEventListener("click",()=>{
 // let msg = "X"
 // const newList = `<li>${msg}</li>` //這裡要用backtick轉成字串
 // list.insertAdjacentHTML("afterbegin",newList)
-
